67f7de5936569a7d950bfe18900c3f85d8a49fda
dexterad22-jpg <dexterad22@gmail.com>
2026-05-21 23:07:03 +0100

    Mise à jour des produits depuis le panneau admin

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('unhandledrejection', function(e) {
 });
 
 const products = [
-    { id: 1, brand: "Jean Paul Gaultier", name: "Le Beau Le Parfum", category: "oriental", desc: "Version intense et boisée au coco et à la fève tonka.", sizeLabel: "100mll", price: 22000, note: 4.5, badge: "Indisponible", image: "jpg-lebeau.jpg" },
+    { id: 1, brand: "Jean Paul Gaultier", name: "Le Beau Le Parfum", category: "oriental", desc: "Version intense et boisée au coco et à la fève tonka.", sizeLabel: "100ml", price: 22000, note: 4.5, badge: "Indisponible", image: "jpg-lebeau.jpg" },
     { id: 2, brand: "Jean Paul Gaultier", name: "Le Beau Paradise Garden", category: "frais", desc: "Un boisé aquatique vert à la noix de coco, figue et santal.", sizeLabel: "125ml", price: 23000, note: 4.5, badge: "Indisponible", image: "jpg-paradise.jpg" },
     { id: 3, brand: "Valentino", name: "Born in Roma Purple Melancholia", category: "boise", desc: "Un boisé aromatique à la cardamome, coco et amberwood.", sizeLabel: "125ml", price: 25000, note: 4.5, badge: "Indisponible", image: "val-purple.jpg" },
     { id: 4, brand: "Rasasi", name: "Hawas Ice", category: "frais", desc: "Un aromatique frais à la pomme, bergamote, prune et musc.", sizeLabel: "125ml", price: 18000, note: 4.5, badge: "Indisponible", image: "hawas-ice.jpg" },
